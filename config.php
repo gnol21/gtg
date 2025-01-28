f89995c922cfb8a4575634b44e4e1a6837b6c9a9
gnol21 <119917889+gnol21@users.noreply.github.com>
2025-01-28 12:04:04 +0800

    Update config.php

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ class Connect extends PDO{
 	
 	public function __construct(){
 		
-		parent::__construct("mysql:host=localhost;dbname=u822511354_mydata",'u822511354_long','@Empasis52186',array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+		parent::__construct("mysql:host=sql107.infinityfree.com;dbname=if0_37839690_mydata",'if0_37839690','empasis52186',array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 		
 		$this->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 		$this->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
